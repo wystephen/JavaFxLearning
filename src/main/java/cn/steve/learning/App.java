03344fb726c2e2f38a0d50b8f970d002e4c485c0
wang stephen <551619855@qq.com>
2017-11-16 21:16:44 +0800

    Initial commit

---
+++ src/main/java/cn/steve/learning/App.java
@@ -0,0 +1,13 @@
+package cn.steve.learning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
